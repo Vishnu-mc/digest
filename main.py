5cae454af2b7947f77986ad731ba3ca81b81ca0f
Vishnu-mc <93871429+Vishnu-mc@users.noreply.github.com>
2022-01-01 16:39:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Welcome to my computer quiz!")
+playing=input("Do you want to play?")
+if playing!="yes":
+    quit()
+print("okey! Let's play:)")
+score=0
+answer = input("what does cpu stands for?")
+if answer.lower()=="central processing unit":
+    print("correct!")
+    score+=1
+else:
+    print("Incorrect")
+answer = input("What does GPU stands for?")
+if answer.lower()== "graphics processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+answer = input("what does RAM stands for?")
+if answer.lower()=="random acess memory":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+answer = input("PSU?")
+if answer.lower()=="power supply unit":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+print("You got "+str(score)+" questions currect!")
